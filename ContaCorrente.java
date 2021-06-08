7b04c42928e38e6e5c0e86dd160d08fc6e1e67d8
PierrySampaio <80012459+PierrySampaio@users.noreply.github.com>
2021-06-08 15:15:24 -0300

    Conta corrente comit

---
+++ ContaCorrente.java
@@ -0,0 +1,21 @@
+public class ContaCorrente extends Conta {
+    
+    public ContaCorrente(int numero, double saldo){
+        super( numero, saldo);
+    
+    }
+
+    @Override
+    public String toString() {
+        
+        return " Conta Corrente " + super.toString();
+    }
+
+    
+
+
+
+    
+
+    
+}
