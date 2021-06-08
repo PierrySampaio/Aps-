a74ea92225920a65ec19f7c94255b9bf909485f1
PierrySampaio <80012459+PierrySampaio@users.noreply.github.com>
2021-06-08 15:17:39 -0300

    Inicio Conta especial

---
+++ ContaEspecial.java
@@ -0,0 +1,11 @@
+public class ContaEspecial extends Conta {
+    private double limite;
+
+    public ContaEspecial(int numero, double saldo, double limite) {
+        super(numero, saldo);
+        this.limite = limite;
+    }
+    
+  
+    
+}
