38271ae1b38c3d80fef07fe9b51ce4ba8f68a2f5
PierrySampaio <80012459+PierrySampaio@users.noreply.github.com>
2021-06-08 16:05:32 -0300

    Add opções

--- AppConta.java
+++ AppConta.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class AppConta {
     public static void main(String[] args) {
-          
-        int opcao;
+        GerenciaConta gerencia = new GerenciaConta();    
+        Scanner teclado = new Scanner(System.in);        
+        int opcao, opcao2;
         int numero;
         double saldo, limite,taxa;
         Conta conta;
@@ -19,31 +20,89 @@ public class AppConta {
         opcao = Integer.parseInt(teclado.nextLine());
 
         switch (opcao) {
-          
-            
-                   case 1:
+            case 1:
+                System.out.println("Qual Conta deseja adicionar?");
+                System.out.println("1-Conta Correte");
+                System.out.println("2-Conta Especial");
+                System.out.println("3-Conta Poupança");
+                opcao2 = Integer.parseInt(teclado.nextLine());
+                switch (opcao2) {
+
+                    case 1://Conta Corrente
                         
-                    break;
+                        System.out.println("Qual o Numero da Conta?");
+                        numero = Integer.parseInt(teclado.nextLine());
+                        System.out.println("Qual o Saldo inicial da Conta?");
+                        //saldo = Double.parseDouble(teclado.nextLine());
+                        teclado.nextLine();
+                        saldo = teclado.nextDouble();
+                        conta = new ContaCorrente(numero, saldo);
+                        gerencia.adicionar(conta);
+                        break;
                     
-                    case 2:
-                    
-                    break;
-                 
-                    case 3:
-                    
-                    break;
-                    
-                    case 4:
-             
-                    break;
+                    case 2://Conta  Especial
+                        System.out.println("Qual o Numero da Conta?");
+                        numero = Integer.parseInt(teclado.nextLine());
+                        System.out.println("Qual o Saldo inicial da Conta?");
+                        saldo = Double.parseDouble(teclado.nextLine());
+                        System.out.println("Qual o Limite adicional para a Conta?");
+                        limite = Double.parseDouble(teclado.nextLine());
+                        conta = new ContaEspecial(numero, saldo, limite);
+                        gerencia.adicionar(conta);
+                        break;
+
+                    case 3://Conta Poupanca
+                        System.out.println("Qual o Numero da Conta?");
+                        numero = Integer.parseInt(teclado.nextLine());
+                        System.out.println("Qual o Saldo inicial da Conta?");
+                        saldo = Double.parseDouble(teclado.nextLine());
+                        System.out.println("Qual a taxa a ser cobrada na Conta?");
+                        taxa = Double.parseDouble(teclado.nextLine());
+                        conta = new ContaPoupança(numero, saldo, taxa);
+                        gerencia.adicionar(conta);
+                        break;
+
+                    default:
+                        
+                        System.out.println("Opção inválida");
+
+                        break;
+                }
+
+
+                break;
         
-                    case 5:
+            case 2:
+                System.out.println("Qual o numero da Conta a ser Removida?");
+                numero = Integer.parseInt(teclado.nextLine());
+                ;
+                if (gerencia.remover(numero)) {
+                System.out.println("removida com sucesso.");
+                } else {
+                System.out.println("Erro. Conta não encontrada.");
+                }
                 
-                    break;
-                    
-                    case 6:
+                break;
+        
+            case 3:
+
+                System.out.println("Lista de Contas:");
+                System.out.println(gerencia.lista());
+                break;
+        
+            case 4:
+                System.out.println("Qual o numero da Conta para o Saque?");
+                numero = teclado.nextInt();
                 
-                    break;
+                
+                break;
+        
+            case 5:
+                
+                break;
+            case 6:
+                
+                break;
         
             default:
                 
